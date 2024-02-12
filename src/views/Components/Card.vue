8babcd2c3ca432f4860819869cfe453b57c67cbc
SmallTeddy <zhangzeming1212@gmail.com>
2024-02-13 01:07:05 +0800

    feat(card): add poker card

---
+++ src/views/Components/Card.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { getAssetsFile} from "@/utils";
+
+type CardType = {
+  number: 'A' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | 'J' | 'Q' | 'K'
+  color: 'heart' | 'spade' | 'club' | 'diamond'
+}
+
+withDefaults(defineProps<{
+  cardInfo: CardType
+}>(), {
+  cardInfo: () => null
+})
+</script>
+
+<template>
+  <div
+      w-145px
+      h-221px
+      border-rd-8px
+      :style="{
+        backgroundImage: `url(${getAssetsFile('poker-all-card.png')})`,
+        backgroundPosition: '-1px -1px'
+      }"
+  ></div>
+</template>
--- src/views/Game/GameMain.vue
+++ src/views/Game/GameMain.vue
@@ -1,10 +1,11 @@
 <script lang="ts" setup>
+import Card from '@/views/Components/Card.vue'
 </script>
 
 <template>
   <div h-full w-screen color-white p8 flex="~ col" gap-8>
     <div h-full w-full of-hidden flex-center border="1px dashed #666" bg-mask border-rd-2>
-      测试区域
+      <Card :card-info="{ number: 'A', color: 'club' }" />
     </div>
   </div>
 </template>
