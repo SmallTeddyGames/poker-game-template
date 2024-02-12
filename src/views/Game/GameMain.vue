51c2fa397f4d8b9a76f31286c41fc5e26b70342d
SmallTeddy <zhangzeming1212@gmail.com>
2024-02-13 00:09:39 +0800

    feat(svg): add poker svg assets

--- src/views/Game/GameMain.vue
+++ src/views/Game/GameMain.vue
@@ -1,5 +1,10 @@
+<script lang="ts" setup>
+</script>
+
 <template>
   <div h-full w-screen color-white p8 flex="~ col" gap-8>
-    <div h-full w-full of-hidden flex-center border="1px dashed #666" bg-mask border-rd-2>卡牌区域</div>
+    <div h-full w-full of-hidden flex-center border="1px dashed #666" bg-mask border-rd-2>
+      测试区域
+    </div>
   </div>
 </template>
